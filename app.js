01501801324ddf40817a4652c18fdde1e510ed26
amuo <amrinderbalhar@gmail.com>
2021-06-06 17:12:12 +0530

    included updated for mobile

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const hostname = '192.168.1.3';
 
 mongoose.connect("mongodb+srv://amuo:amuo12345@cluster0.e6cjg.mongodb.net/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -117,6 +118,6 @@ app.get("/post/:postName", function (req, res) {
 
 
 
-app.listen(process.env.PORT, function () {
-  console.log("Server started on port 3000");
+app.listen( process.env.PORT, function () {
+  console.log("Server started on  3000");
 });
